359f0818711f34a268c1b3b7fd462c0f7ef64c2d
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2024-10-21 21:38:57 -0500

    almostdone

--- tindex.js
+++ tindex.js
@@ -1,41 +1,57 @@
 const freelancers = [
-    { name: "Dr. Slice", price: 25, occupation: "gardener" },
-    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-    { name: "Prof. Prism", price: 81, occupation: "teacher" },
-    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-    { name: "Prof. Spark", price: 76, occupation: "programmer" },
-    { name: "Dr. Wire", price: 47, occupation: "teacher" },
-    { name: "Prof. Goose", price: 72, occupation: "driver" },
+    { name: "Slice", price: 25, occupation: "gardener" },
+    { name: "Pressure", price: 51, occupation: "programmer" },
+    { name: "Possibility", price: 43, occupation: "teacher" },
+    { name: "Prism", price: 81, occupation: "teacher" },
+    { name: "Impulse", price: 43, occupation: "teacher" },
+    { name: "Spark", price: 76, occupation: "programmer" },
+    { name: "Wire", price: 47, occupation: "teacher" },
+    { name: "Goose", price: 72, occupation: "driver" },
   ];
-//   const names= ["Dr. Slice", "Dr. Pressure", "Prof. Possibility", "Prof. Prism", "Dr. Ipulse", "Prof. Spark", "Dr. Wire", "Prof. Goose"]
-//   const occupations= ["gardener", "programmer", "teacher", "driver"]
-//   function randFreelancer(freelancers){
-//     const newString = `${freelancers.name} is a ${freelancers.occupation}`;
-//   }
-//   console.log(randFreelancer)
+
 console.log(freelancers)
 
 const body = document.querySelector("body")
 body.style.backgroundColor= "pink"
 
-const init=()=>{
-    const section = document.createElement("section");
-    for (let i=0; i<freelancers.length; i++){
-        const element = freelancers[i];
-        const div= document.createElement("div");
-        const h3 = document.createElement("h3");
-        h3.style.backgroundColor= "pink";
-        h3.innerText= element.name;
-        // h3.innerText= element.price;
-        // h3.innerText= element.occupation;
-        const h4 = document.createElement("h4");
-        h4.innerText= "$"+ element.price;
-        div.append(h4)
-        div.append(h3)
-        section.append(div)      
-    }
-    body.append(section)
-}
+const topHeader= document.createElement("h1")
+topHeader.textContent= "Freelancer Forum"
+document.body.appendChild(topHeader)
 
+const init=()=>{
+    const outerSection = document.createElement("div");
+    for (let i=0; i<freelancers.length; i++){
+        setTimeout(()=>{
+        // const avgTitle = document.createElement("h6");
+        
+        // const h3 = document.createElement("h3");
+        // const infoDiv= document.createElement("div");
+        // infoDiv.id= 'info'
+        // const nameDiv= document.createElement("div");
+        // nameDiv.id= 'name'
+        // const occDiv= document.createElement("div");
+        // occDiv.id= 'occupation'
+        // const priceDiv= document.createElement("div");
+        // priceDiv.id= 'price'
+        // infoDiv.appendChild ('nameDiv');
+        // h3.style.backgroundColor= "black";
+        // h3.innerText= element.name;
+        // h3.textContent= "Available freelancers:"
+        const randIndex = Math.floor(Math.random()*freelancers.length)
+        const randName = freelancers[randIndex]
+        const element = freelancers[i];
+        const title= document.createElement("h1");
+       
+        title.textContent= randName.name;
+       
+        outerSection.appendChild(title);
+        document.body.appendChild(outerSection); 
+    }, i*2000); 
+     }   
+    
+}
 init();
+
+
+// avgTitle.textContent= `The average starting price is `
+//         body.append(avgTitle)
