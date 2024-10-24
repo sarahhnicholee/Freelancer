3c1e03d80f864b4d6810f5e85f4faeca556fb353
Torie Kim <torie.kim@fullstackacademy.com>
2024-10-23 22:29:46 -0400

    torie demo during osh

--- tindex.js
+++ tindex.js
@@ -1,57 +1,63 @@
 const freelancers = [
-    { name: "Slice", price: 25, occupation: "gardener" },
-    { name: "Pressure", price: 51, occupation: "programmer" },
-    { name: "Possibility", price: 43, occupation: "teacher" },
-    { name: "Prism", price: 81, occupation: "teacher" },
-    { name: "Impulse", price: 43, occupation: "teacher" },
-    { name: "Spark", price: 76, occupation: "programmer" },
-    { name: "Wire", price: 47, occupation: "teacher" },
-    { name: "Goose", price: 72, occupation: "driver" },
-  ];
+  { name: 'Slice', price: 25, occupation: 'gardener' },
+  { name: 'Pressure', price: 51, occupation: 'programmer' },
+  { name: 'Possibility', price: 43, occupation: 'teacher' },
+  { name: 'Prism', price: 81, occupation: 'teacher' },
+  { name: 'Impulse', price: 43, occupation: 'teacher' },
+  { name: 'Spark', price: 76, occupation: 'programmer' },
+  { name: 'Wire', price: 47, occupation: 'teacher' },
+  { name: 'Goose', price: 72, occupation: 'driver' },
+];
 
-console.log(freelancers)
+const names = ['Abby', 'Brian', 'Courtney', 'David'];
+const occupations = ['server', 'nurse', 'clerk', 'designer'];
 
-const body = document.querySelector("body")
-body.style.backgroundColor= "pink"
+const body = document.querySelector('body');
+body.style.backgroundColor = 'pink';
 
-const topHeader= document.createElement("h1")
-topHeader.textContent= "Freelancer Forum"
-document.body.appendChild(topHeader)
+const table = document.querySelector('table');
+const tableBody = document.querySelector('#table-body');
 
-const init=()=>{
-    const outerSection = document.createElement("div");
-    for (let i=0; i<freelancers.length; i++){
-        setTimeout(()=>{
-        // const avgTitle = document.createElement("h6");
-        
-        // const h3 = document.createElement("h3");
-        // const infoDiv= document.createElement("div");
-        // infoDiv.id= 'info'
-        // const nameDiv= document.createElement("div");
-        // nameDiv.id= 'name'
-        // const occDiv= document.createElement("div");
-        // occDiv.id= 'occupation'
-        // const priceDiv= document.createElement("div");
-        // priceDiv.id= 'price'
-        // infoDiv.appendChild ('nameDiv');
-        // h3.style.backgroundColor= "black";
-        // h3.innerText= element.name;
-        // h3.textContent= "Available freelancers:"
-        const randIndex = Math.floor(Math.random()*freelancers.length)
-        const randName = freelancers[randIndex]
-        const element = freelancers[i];
-        const title= document.createElement("h1");
-       
-        title.textContent= randName.name;
-       
-        outerSection.appendChild(title);
-        document.body.appendChild(outerSection); 
-    }, i*2000); 
-     }   
-    
-}
+const topHeader = document.createElement('h1');
+topHeader.textContent = 'Freelancer Forum';
+document.body.appendChild(topHeader);
+
+const init = () => {
+  // First, we need to map through or loop through our freelancer array to render each freelancer
+  const freelancerElements = freelancers.map((freelancer) => {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td>${freelancer.name}</td>
+      <td>${freelancer.occupation}</td>
+      <td>$${freelancer.price}</td>
+    `;
+    return newRow;
+  });
+
+  tableBody.replaceChildren(...freelancerElements);
+};
+
+const addFreelancerIntervalId = setInterval(() => {
+  const randIndex = Math.floor(Math.random() * names.length);
+  const randName = names[randIndex];
+  const randOccupation =
+    occupations[Math.floor(Math.random() * occupations.length)];
+  const randPrice = Math.floor(Math.random() * 100);
+
+  const newFreelancer = {
+    name: randName,
+    occupation: randOccupation,
+    price: randPrice,
+  };
+
+  // console.log('new freelancer:', newFreelancer);
+  freelancers.push(newFreelancer);
+  init();
+
+  // Clear setInterval once freelancers length is equal to max length
+  if (freelancers.length === 13) {
+    clearInterval(addFreelancerIntervalId);
+  }
+}, 3000);
+
 init();
-
-
-// avgTitle.textContent= `The average starting price is `
-//         body.append(avgTitle)
