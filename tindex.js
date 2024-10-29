01f0f570f7dfd78515479cde30f2cd4755186af5
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2024-10-28 22:25:08 -0500

    DONEEE

--- tindex.js
+++ tindex.js
@@ -1,57 +1,75 @@
 const freelancers = [
-    { name: "Slice", price: 25, occupation: "gardener" },
-    { name: "Pressure", price: 51, occupation: "programmer" },
-    { name: "Possibility", price: 43, occupation: "teacher" },
-    { name: "Prism", price: 81, occupation: "teacher" },
-    { name: "Impulse", price: 43, occupation: "teacher" },
-    { name: "Spark", price: 76, occupation: "programmer" },
-    { name: "Wire", price: 47, occupation: "teacher" },
-    { name: "Goose", price: 72, occupation: "driver" },
+    { name: "Iggy", price: 25, occupation: "gardener" },
+    { name: "Maddy", price: 51, occupation: "programmer" },
+    { name: "Britt", price: 43, occupation: "teacher" },
+    { name: "Angela", price: 81, occupation: "teacher" },
+    { name: "Todd", price: 43, occupation: "teacher" },
+    { name: "Mark", price: 76, occupation: "programmer" },
+    { name: "Melony", price: 47, occupation: "teacher" },
+    { name: "Adam", price: 72, occupation: "driver" },
   ];
+const names= ['Gabby', 'Chandra', 'Kimberly', 'Bryan']
+const occupations= ['banker', 'pipeliner', 'secretary', 'cop']
 
-console.log(freelancers)
-
-const body = document.querySelector("body")
+const body = document.querySelector('body')
 body.style.backgroundColor= "pink"
 
-const topHeader= document.createElement("h1")
-topHeader.textContent= "Freelancer Forum"
-document.body.appendChild(topHeader)
+
+
+// const topHeader= document.createElement("h1")
+// topHeader.textContent= "Freelancer Forum"
+// document.body.append(topHeader)
+
+const table = document.querySelector('table');
+const tableBody= document.querySelector('#tableBody');
+// table.append(tableBody)
+// body.append(table)
+
 
 const init=()=>{
-    const outerSection = document.createElement("div");
-    for (let i=0; i<freelancers.length; i++){
-        setTimeout(()=>{
-        // const avgTitle = document.createElement("h6");
-        
-        // const h3 = document.createElement("h3");
-        // const infoDiv= document.createElement("div");
-        // infoDiv.id= 'info'
-        // const nameDiv= document.createElement("div");
-        // nameDiv.id= 'name'
-        // const occDiv= document.createElement("div");
-        // occDiv.id= 'occupation'
-        // const priceDiv= document.createElement("div");
-        // priceDiv.id= 'price'
-        // infoDiv.appendChild ('nameDiv');
-        // h3.style.backgroundColor= "black";
-        // h3.innerText= element.name;
-        // h3.textContent= "Available freelancers:"
-        const randIndex = Math.floor(Math.random()*freelancers.length)
-        const randName = freelancers[randIndex]
-        const element = freelancers[i];
-        const title= document.createElement("h1");
-       
-        title.textContent= randName.name;
-       
-        outerSection.appendChild(title);
-        document.body.appendChild(outerSection); 
-    }, i*2000); 
-     }   
-    
+    const freelancerElements = freelancers.map((freelancer)=> {
+      const newRow= document.createElement('tr');
+      newRow.innerHTML=`
+      <td>${freelancer.name}</td>
+      <td>${freelancer.occupation}</td>
+      <td>$${freelancer.price}</td>
+      `;
+      return newRow;
+    });
+    tableBody.replaceChildren(...freelancerElements);
+  };
+
+
+
+const addFreelancerIntervalId= setInterval(()=>{
+  const randIndex = Math.floor(Math.random()*names.length);
+  const randName = names[randIndex];
+  const randOcc= occupations[Math.floor(Math.random()* occupations.length)];
+  const randPrice = Math.floor(Math.random() * 100);
+  // const randIPrice= Math.floor(Math.random()*freelancers.price)
+  // const randPri= freelancers[randIPrice];
+     
+const newFreelancer={
+  name: randName,
+  occupation: randOcc,
+  price: randPrice,
+
+  
+};
+
+let sum= 438;
+sum+= randPrice;
+const avPrice= sum/ freelancers.length
+console.log(`The av price for all the current freelancers is $${avPrice}`);
+freelancers.push(newFreelancer);
+init()
+
+if (freelancers.length ===13){
+  clearInterval(addFreelancerIntervalId);
 }
-init();
+},3000);
+init()
 
-
-// avgTitle.textContent= `The average starting price is `
-//         body.append(avgTitle)
+// const sum= randPrice.reduce((total,price)=> total + price, 0);
+// const avPrice= sum/ randPrice.length
+// console.log('The av price is $${avPrice}');
